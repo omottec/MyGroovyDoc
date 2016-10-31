4afae1992bd25102394064aa10e38f6f0886c5f4
omottec <omottec@gmail.com>
2016-10-31 11:53:13 +0800

    Comments

--- src/Syntax/ch01/Comments.groovy
+++ src/Syntax/ch01/Comments.groovy
@@ -1,2 +1,17 @@
 // a standalone single line comment
 println "hello" // a comment till the end of the line
+
+println "hello" /* a multiline comment starting
+                   at the end of a statement*/
+
+println 1 /* one */ + 2 /* two */
+
+/**
+ * A Class description
+ */
+
+Class Comments {
+
+    /** the name of the person */
+
+}
