d20b68f4427302ffa1b607c7b32497b80ac57ae6
omottec <omottec@gmail.com>
2016-11-09 11:06:31 +0800

    Parameters ch0201 Normal parameters

---
+++ src/Closures/ch0201.groovy
@@ -0,0 +1,24 @@
+package Closures
+
+/**
+ * Created by qinbingbing on 11/9/16.
+ * Normal parameters
+ */
+
+def closureWithOneArg = { str -> str.toUpperCase() }
+assert closureWithOneArg('groovy') == 'GROOVY'
+
+def closureWithOneArgAndExplicitType = { String str -> str.toUpperCase() }
+assert closureWithOneArgAndExplicitType('groovy') == 'GROOVY'
+
+def closureWithTwoArgs = { a,b -> a+b }
+assert closureWithTwoArgs(1,2) == 3
+
+def closureWithTwoArgsAndExplicitTypes = { int a, int b -> a+b }
+assert closureWithTwoArgsAndExplicitTypes(1,2) == 3
+
+def closureWithTwoArgsAndOptionalTypes = { a, int b -> a+b }
+assert closureWithTwoArgsAndOptionalTypes(1,2) == 3
+
+def closureWithTwoArgsAndDefaultValue = { int a, int b=2 -> a+b }
+assert closureWithTwoArgsAndDefaultValue(1) == 3
