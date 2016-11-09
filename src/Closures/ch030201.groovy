c8c07703aea93f3008da7edbdcccf4dd88b57ac6
omottec <omottec@gmail.com>
2016-11-09 11:46:21 +0800

    3.2.1. The meaning of this

---
+++ src/Closures/ch030201.groovy
@@ -0,0 +1,54 @@
+package Closures
+
+/**
+ * Created by qinbingbing on 11/9/16.
+ * The meaning of this
+ */
+
+class Enclosing {
+    void run() {
+        def whatIsThisObject = { getThisObject() }
+        assert whatIsThisObject() == this
+        def whatIsThis = { this }
+        assert whatIsThis() == this
+    }
+}
+
+class EnclosedInInnerClass {
+    class Inner {
+        Closure cl = { this }
+    }
+
+    void run() {
+        def inner = new Inner();
+        assert inner.cl() == inner
+    }
+}
+
+class NestedClosures {
+    void run() {
+        def nestedClosures = {
+            def cl = { this }
+            cl()
+        }
+        assert nestedClosures() == this
+    }
+}
+
+class Person {
+    String name
+    int age
+    String toString() { "$name is $age years old" }
+
+    String dump() {
+        def cl = {
+            String msg = this.toString()
+            println msg
+            msg
+        }
+        cl()
+    }
+}
+
+def p = new Person(name:'Janice', age:74)
+assert p.dump() == 'Janice is 74 years old'
