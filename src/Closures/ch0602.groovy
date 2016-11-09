63536c03d54a374545f5ad89702ba23119f8a37a
omottec <omottec@gmail.com>
2016-11-09 16:07:31 +0800

    6.2. Memoization

---
+++ src/Closures/ch0602.groovy
@@ -0,0 +1,13 @@
+package Closures
+
+/**
+ * Created by qinbingbing on 11/9/16.
+ * Memoization
+ */
+
+def fib
+fib = { long n -> n < 2 ? n : fib(n-1) + fib(n-2) }
+assert fib(15) == 610 // slow!
+
+fib = { long n -> n < 2 ? n : fib(n-1) + fib(n-2) }.memoize()
+assert fib(25) == 75025 // fast!
