a26990963a20c04c8bcbe74b12c02396ff9c98f1
omottec <omottec@gmail.com>
2016-11-09 11:18:27 +0800

    Parameters ch0203 Varargs

---
+++ src/Closures/ch0203.groovy
@@ -0,0 +1,16 @@
+package Closures
+
+/**
+ * Created by qinbingbing on 11/9/16.
+ * Varargs
+ */
+
+def concat1 = { String... args -> args.join('') }
+assert concat1('abc','def') == 'abcdef'
+def concat2 = { String[] args -> args.join('') }
+assert concat2('abc','def') == 'abcdef'
+
+def multiConcat = {int n, String... args ->
+    args.join('')*n
+}
+assert multiConcat(2, 'abc', 'def') == 'abcdefabcdef'
