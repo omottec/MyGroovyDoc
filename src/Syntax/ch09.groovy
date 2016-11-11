a24f241f9d1da1e62235dffb99370c0a599484c7
omottec <omottec@gmail.com>
2016-11-11 11:31:01 +0800

    ch09 Maps

---
+++ src/Syntax/ch09.groovy
@@ -0,0 +1,36 @@
+package Syntax
+
+/**
+ * Created by qinbingbing on 11/11/16.
+ * Maps
+ */
+
+def colors = [red: '#FF0000', green: '#00FF00', blue: '#0000FF']
+
+assert colors['red'] == '#FF0000'
+assert colors.green == '#00FF00'
+
+colors['pink'] = '#FF00FF'
+colors.yellow = '#FFFF00'
+
+assert colors.pink == '#FF00FF'
+assert colors['yellow'] == '#FFFF00'
+
+assert colors instanceof LinkedHashMap
+
+assert colors.unknown == null
+
+
+def numbers = [1: 'one', 2: 'two']
+assert numbers[1] == 'one'
+
+def key = 'name'
+def person = [key: 'Guillaume']
+
+assert !person.containsKey('name')
+assert person.containsKey('key')
+
+person = [(key): 'Guillaume']
+
+assert person.containsKey('name')
+assert !person.containsKey('key')
