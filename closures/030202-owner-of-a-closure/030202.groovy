7460ceb0e53c5997474c49c85c5ecf6eef760d48
omottec <omottec@gmail.com>
2016-12-01 14:37:28 +0800

    030202-owner-of-a-closure

--- closures/030202-owner-of-a-closure/030202.groovy
+++ closures/030202-owner-of-a-closure/030202.groovy
@@ -5,6 +5,32 @@
 class Enclosing {
     void run() {
         def whatIsOwnerMethod = { getOwner() }
-        assert whatIsOwnerMethod == this
+        assert whatIsOwnerMethod() == this
+        def whatIsOwner = { owner }
+        assert whatIsOwner() == this
     }
 }
+new Enclosing().run()
+
+class EnclosedInnerClass {
+    class Inner {
+        Closure cl = { owner }
+    }
+
+    void run() {
+        def inner = new Inner()
+        assert inner.cl() == inner
+    }
+}
+new EnclosedInnerClass().run()
+
+class NestedClosures {
+    void run() {
+        def nestedClosures = {
+            def cl = { owner }
+            cl()
+        }
+        assert nestedClosures() == nestedClosures
+    }
+}
+new NestedClosures().run()
